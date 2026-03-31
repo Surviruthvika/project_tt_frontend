6033a1cfdb00289caddaeaeab83855f99dde8f65
Survi Ruthvika <surviruthvika@gmail.com>
2026-04-01 01:05:47 +0530

    frontend ready

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://au-backend-1-1jf0.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
